ca142c39c1c778c0bcdd3cf19ff63dfadb645c21
Gideon Nnalue <emekannalue@gmail.com>
2021-02-10 13:19:45 +0100

    env issues

--- DB/db.js
+++ DB/db.js
@@ -2,6 +2,8 @@ const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
   process.env.DATABASE_URL,
+    process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD,
   {
     host: process.env.DATABASE_HOST,
     dialect: "postgres",

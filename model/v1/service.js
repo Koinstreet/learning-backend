16a1fcea8165d46a60c8d441986b7a5f5c1c3e4b
pextech <mcstain1639@gmail.com>
2021-08-03 18:51:08 +0200

    added service

--- model/v1/service.js
+++ model/v1/service.js
@@ -3,12 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const serviceSchema = new Schema({
-  cost: {
-    type: Number,
-  },
-  amount: {
-    type: Number,
-  },
   project_name: {
     type: String,
     required: true,
@@ -16,7 +10,41 @@ const serviceSchema = new Schema({
   industry: {
     type: String,
   },
-  email: {
+  project_budget: {
+    type: String,
+  },
+  budget_currency: {
+    type: String,
+  },
+  front_end_tags: {
+    type: Array,
+  },
+  project_stacks: {
+    type: Array,
+  },
+  PM_fullName: {
+    type: String,
+  },
+  Agree_terms: {
+    type: Boolean,
+    default: false,
+  },
+  project_size: {
+    type: String,
+  },
+  project_details: {
+    type: String,
+  },
+  webiste: {
+    type: String,
+  },
+  project_tags: {
+    type: Array,
+  },
+  PM_email: {
+    type: String,
+  },
+  contact_email: {
     type: String,
     required: true,
   },
@@ -24,7 +52,10 @@ const serviceSchema = new Schema({
     type: Number,
   },
   launch_date: {
-    type: Date,
+    type: String,
+  },
+  pay_option: {
+    type: String,
   },
 }, {
   timestamps: true
--- validators/service.js
+++ validators/service.js
@@ -4,13 +4,13 @@ const _ = require("lodash");
 const validateService = (data) => {
   let errors = {};
   data.project_name = data.project_name ? data.project_name : "";
-  data.email = data.email ? data.email : "";
+  data.contact_email = data.contact_email ? data.contact_email : "";
 
   if (validator.isEmpty(data.project_name)) {
     errors.project_name = "project_name is required";
   }
-  if (validator.isEmpty(data.email)) {
-    errors.email = "email is required";
+  if (validator.isEmpty(data.contact_email)) {
+    errors.contact_email = "contact_email is required";
   }
   return { errors, isValid: _.isEmpty(errors) };
 };

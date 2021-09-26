5ed4f7530f617a8f3706f3171a056579f537c327
pextech <mcstain1639@gmail.com>
2021-09-26 19:38:35 +0200

    removed module validators

--- validators/module.js
+++ validators/module.js
@@ -3,11 +3,6 @@ const _ = require("lodash");
 
 const validateModule = (data) => {
   let errors = {};
-  data.name = data.name ? data.name : "";
-
-  if (validator.isEmpty(data.name)) {
-    errors.name = "Module name is required";
-  }
   return { errors, isValid: _.isEmpty(errors) };
 };
 

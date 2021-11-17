ccafc39469489b15a2607f6e0f754cdda5d61e0c
pextech <mcstain1639@gmail.com>
2021-11-17 21:31:15 +0200

    added error handler

--- app.js
+++ app.js
@@ -110,4 +110,14 @@ app.use('/api/v1/wallets', walletRouters)
 app.use('/api/v1/notification', notificationsRouters)
 app.use('/', socialRoutes)
 
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
+
+
 module.exports = app;

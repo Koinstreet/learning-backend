32f20f760994e4892d3e53b8c50b4b60e85c26ad
pextech <mcstain1639@gmail.com>
2021-12-09 12:41:55 +0200

    all fixed

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
-import cookieSession from 'cookie-session';
+const cors = require("cors");
+import cookieSession from "cookie-session";
 import "@babel/polyfill";
-import passport from 'passport';
+import passport from "passport";
 
-import socialAuthRoutes from './routes/socialAuthRoutes';
-import { getSearch } from './controllers/v1/search';
+import socialAuthRoutes from "./routes/socialAuthRoutes";
+import { getSearch } from "./controllers/v1/search";
 
 // ROUTES
-const userRoutes = require('./routes/userRoutes');
-const courseRoutes = require('./routes/courseRoutes');
-const subscriberRoutes = require('./routes/subscriberRoutes');
-const jobsRoutes = require('./routes/jobsRoutes');
-const projectRoutes = require('./routes/projectRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const mentorRoutes = require('./routes/mentorRoutes');
-const menteeRoutes = require('./routes/menteeRoutes');
-const mentorshipRoutes = require('./routes/mentorShip');
-const startupRoutes = require('./routes/startup');
-const locationRoutes = require('./routes/locationRoutes');
-const serviceRoutes = require('./routes/serviceRoutes');
-const chapterStatsRoutes = require('./routes/chapterRouters');
-const fundedRoutes = require('./routes/fundedRoutes');
-const minorityEarnedRoutes = require('./routes/minority_earnedRoutes');
-const proposalRoutes = require('./routes/proposalRoutes');
-const proposalStatusRoutes = require('./routes/proposalStatusRoutes');
-const proposalViewRoutes = require('./routes/proposalViews');
-const chapterRoutes = require('./routes/chapter');
-const socialRoutes = require('./routes/socialAuthRoutes');
-const savedEventsRoutes = require('./routes/savedEventsRoutes');
-const savedJobsRoutes = require('./routes/savedJobsRoutes')
-const companiesRoutes = require('./routes/companiesRouters');
-const chapterToolKitRoutes = require('./routes/chapterToolKit');
-const joinChapterRoutes = require('./routes/joinChapterRoutes');
-const chatRoutes = require('./routes/chatRoutes');
-const chatMessageRoutes = require('./routes/chatMessageRoutes');
-const learnRoutes = require('./routes/learnRoutes');
-const easyApplyRoutes = require('./routes/easyApplyRoutes');
-const claimedProjects = require('./routes/claimedProjects');
-const upvotes = require('./routes/upvotes');
-const downvotes = require('./routes/downVotes');
-const walletRouters = require('./routes/walletRouters');
-const notificationsRouters = require('./routes/notifications');
-const sidebarMenuRouters = require('./routes/SidebarMenu');
-const childLinksRoutes = require('./routes/childLinks');
-const suggestionsRoutes = require('./routes/suggestionsRoutes');
-const certificateRoutes = require('./routes/certificate');
-
-
+const userRoutes = require("./routes/userRoutes");
+const courseRoutes = require("./routes/courseRoutes");
+const subscriberRoutes = require("./routes/subscriberRoutes");
+const jobsRoutes = require("./routes/jobsRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const mentorRoutes = require("./routes/mentorRoutes");
+const menteeRoutes = require("./routes/menteeRoutes");
+const mentorshipRoutes = require("./routes/mentorShip");
+const startupRoutes = require("./routes/startup");
+const locationRoutes = require("./routes/locationRoutes");
+const serviceRoutes = require("./routes/serviceRoutes");
+const chapterStatsRoutes = require("./routes/chapterRouters");
+const fundedRoutes = require("./routes/fundedRoutes");
+const minorityEarnedRoutes = require("./routes/minority_earnedRoutes");
+const proposalRoutes = require("./routes/proposalRoutes");
+const proposalStatusRoutes = require("./routes/proposalStatusRoutes");
+const proposalViewRoutes = require("./routes/proposalViews");
+const chapterRoutes = require("./routes/chapter");
+const socialRoutes = require("./routes/socialAuthRoutes");
+const savedEventsRoutes = require("./routes/savedEventsRoutes");
+const savedJobsRoutes = require("./routes/savedJobsRoutes");
+const companiesRoutes = require("./routes/companiesRouters");
+const chapterToolKitRoutes = require("./routes/chapterToolKit");
+const joinChapterRoutes = require("./routes/joinChapterRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const chatMessageRoutes = require("./routes/chatMessageRoutes");
+const learnRoutes = require("./routes/learnRoutes");
+const easyApplyRoutes = require("./routes/easyApplyRoutes");
+const claimedProjects = require("./routes/claimedProjects");
+const upvotes = require("./routes/upvotes");
+const downvotes = require("./routes/downVotes");
+const walletRouters = require("./routes/walletRouters");
+const notificationsRouters = require("./routes/notifications");
+const sidebarMenuRouters = require("./routes/SidebarMenu");
+const childLinksRoutes = require("./routes/childLinks");
+const suggestionsRoutes = require("./routes/suggestionsRoutes");
+const certificateRoutes = require("./routes/certificate");
 
 const app = express();
 
@@ -58,13 +56,15 @@ if (process.env === "development") {
 
 // Implement Cors
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-app.use(cookieSession({
-  // milliseconds of a day
-  maxAge: 24 * 60 * 60 * 1000,
-  keys: 12345678,
-}));
+app.use(
+  cookieSession({
+    // milliseconds of a day
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: 12345678
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -73,54 +73,67 @@ app.use(passport.session());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => { res.send("working"); res.status(200) });
-
-
+app.get("/", (req, res) => {
+  res.send("working");
+  res.status(200);
+});
 
 app.use(socialAuthRoutes);
 
-
 // INITIALIZE ROUTES
-app.use('/api/v1/user', userRoutes);
-app.use('/api/v1/course', courseRoutes);
-app.use('/api/v1/subscribe', subscriberRoutes);
-app.use('/api/v1/job', jobsRoutes);
-app.use('/api/v1/project', projectRoutes);
-app.use('/api/v1/event', eventRoutes);
-app.use('/api/v1/mentor', mentorRoutes);
-app.use('/api/v1/mentee', menteeRoutes);
-app.use('/api/v1/mentorship', mentorshipRoutes);
-app.use('/api/v1/startup', startupRoutes);
-app.use('/api/v1/location', locationRoutes);
-app.use('/api/v1/service', serviceRoutes);
-app.use('/api/v1/chapter_stats', chapterStatsRoutes);
-app.use('/api/v1/funded', fundedRoutes);
-app.use('/api/v1/minority_earned', minorityEarnedRoutes);
-app.use('/api/v1/proposal', proposalRoutes);
-app.use('/api/v1/chapter', chapterRoutes);
-app.use('/api/v1/saveEvent', savedEventsRoutes);
-app.use('/api/v1/saveJob', savedJobsRoutes);
-app.use('/api/v1/company', companiesRoutes);
-app.use('/api/v1/chapterToolKit', chapterToolKitRoutes);
-app.use('/api/v1/joinChapter', joinChapterRoutes);
-app.use('/api/v1/chat', chatRoutes);
-app.use('/api/v1/chat_message', chatMessageRoutes);
-app.use('/api/v1/learn', learnRoutes);
-app.post('/api/v1/search', getSearch);
-app.use('/api/v1/easyApply', easyApplyRoutes);
-app.use('/api/v1/claimProject', claimedProjects);
-app.use('/api/v1/upVotes', upvotes);
-app.use('/api/v1/downVotes', downvotes);
-app.use('/api/v1/proposalStatus', proposalStatusRoutes)
-app.use('/api/v1/proposalViews', proposalViewRoutes)
-app.use('/api/v1/wallets', walletRouters)
-app.use('/api/v1/notification', notificationsRouters)
-app.use('/', socialRoutes)
-app.use('/api/v1/sidebarmenu', sidebarMenuRouters)
-app.use('/api/v1/childlink', childLinksRoutes)
-app.use('/api/v1/suggestions', suggestionsRoutes)
-app.use('/api/v1/certificate', certificateRoutes)
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/subscribe", subscriberRoutes);
+app.use("/api/v1/job", jobsRoutes);
+app.use("/api/v1/project", projectRoutes);
+app.use("/api/v1/event", eventRoutes);
+app.use("/api/v1/mentor", mentorRoutes);
+app.use("/api/v1/mentee", menteeRoutes);
+app.use("/api/v1/mentorship", mentorshipRoutes);
+app.use("/api/v1/startup", startupRoutes);
+app.use("/api/v1/location", locationRoutes);
+app.use("/api/v1/service", serviceRoutes);
+app.use("/api/v1/chapter_stats", chapterStatsRoutes);
+app.use("/api/v1/funded", fundedRoutes);
+app.use("/api/v1/minority_earned", minorityEarnedRoutes);
+app.use("/api/v1/proposal", proposalRoutes);
+app.use("/api/v1/chapter", chapterRoutes);
+app.use("/api/v1/saveEvent", savedEventsRoutes);
+app.use("/api/v1/saveJob", savedJobsRoutes);
+app.use("/api/v1/company", companiesRoutes);
+app.use("/api/v1/chapterToolKit", chapterToolKitRoutes);
+app.use("/api/v1/joinChapter", joinChapterRoutes);
+app.use("/api/v1/chat", chatRoutes);
+app.use("/api/v1/chat_message", chatMessageRoutes);
+app.use("/api/v1/learn", learnRoutes);
+app.post("/api/v1/search", getSearch);
+app.use("/api/v1/easyApply", easyApplyRoutes);
+app.use("/api/v1/claimProject", claimedProjects);
+app.use("/api/v1/upVotes", upvotes);
+app.use("/api/v1/downVotes", downvotes);
+app.use("/api/v1/proposalStatus", proposalStatusRoutes);
+app.use("/api/v1/proposalViews", proposalViewRoutes);
+app.use("/api/v1/wallets", walletRouters);
+app.use("/api/v1/notification", notificationsRouters);
+app.use("/", socialRoutes);
+app.use("/api/v1/sidebarmenu", sidebarMenuRouters);
+app.use("/api/v1/childlink", childLinksRoutes);
+app.use("/api/v1/suggestions", suggestionsRoutes);
+app.use("/api/v1/certificate", certificateRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error("endpoint Not found...");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
 
 module.exports = app;
-
--- controllers/v1/certificate/index.js
+++ controllers/v1/certificate/index.js
@@ -1,93 +1,137 @@
 const AppError = require("../../../utils/appError");
 const { CREATED, OK } = require("http-status-codes");
-const { successWithData, successNoData } = require("../../../utils/successHandler");
+const {
+  successWithData,
+  successNoData
+} = require("../../../utils/successHandler");
 
 const Course = require("../../../model/v1/Course");
-const Certificate = require("../../../model/v1/certificate")
+const Certificate = require("../../../model/v1/certificate");
 
-const PythonShell = require('python-shell').PythonShell;
-const ipfs = require('ipfs-http-client')
-const client = ipfs.create('https://ipfs.infura.io:5001/api/v0')
-const fs = require('fs')
-const script = "utils/certifcate/gen.py"
+const PythonShell = require("python-shell").PythonShell;
+const ipfs = require("ipfs-http-client");
+const client = ipfs.create("https://ipfs.infura.io:5001/api/v0");
+const fs = require("fs");
+const script = "utils/certifcate/gen.py";
 
 exports.uploadCertificate = async (req, res, next) => {
+  try {
+    const course = await Course.findById(req.body.id);
+    const username = req.user.firstName + " " + req.user.lastName;
 
+    let options = {
+      mode: "text",
+      args: [course.description, username, course.name, course.earn]
+    };
 
-    try {
+    PythonShell.run(script, options, function(err, results) {
+      if (err) throw err;
 
-        const course = await Course.findById(req.body.id);
-        const username = req.user.firstName + " " + req.user.lastName;
+      console.log("certificate " + username + " created");
 
-        let options = {
-            mode: 'text',
-            args: [course.description, username, course.name, course.earn]
-        };
+      console.log("uploading to IPFS...");
+      let path = "utils/certifcate/images/";
 
-        PythonShell.run(script, options, function (err, results) {
-            if (err) throw err;
+      fs.readFile(path + username.replace(/ /g, "_") + ".png", async function(
+        err,
+        file
+      ) {
+        if (err) throw err;
 
-            console.log('certificate ' + username + ' created');
-
-            console.log("uploading to IPFS...");
-            let path = "utils/certifcate/images/"
-
-            fs.readFile(path + username.replace(/ /g, "_") + ".png", async function (err, file) {
-                if (err) throw err;
-
-                let added = await client.add(
-                    file,
-                    {
-                        progress: (prog) => console.log(`received: ${prog}`)
-                    }
-                )
-
-                let url = `https://ipfs.infura.io/ipfs/${added.path}`
-
-                const certificate = new Certificate({
-                    userId: req.user._id,
-                    courseId: course._id,
-                    ipfsURL: url,
-                });
-
-                const newCertificate = await Certificate.create(certificate);
-
-                successWithData(res, OK, "certificate successfully created", newCertificate);
-            });
+        let added = await client.add(file, {
+          progress: prog => console.log(`received: ${prog}`)
         });
 
-    } catch (err) {
-        console.log(err);
-        return AppError.tryCatchError(res, err);
-    }
+        let url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+        const certificate = new Certificate({
+          userId: req.user._id,
+          courseId: course._id,
+          ipfsURL: url
+        });
+
+        const newCertificate = await Certificate.create(certificate);
+
+        successWithData(
+          res,
+          OK,
+          "certificate successfully created",
+          newCertificate
+        );
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    return AppError.tryCatchError(res, err);
+  }
 };
 
-
 exports.getUserCertificate = async (req, res, next) => {
+  try {
+    const userCertificates = await Certificate.find({ userId: req.user.id })
+      .populate("userId")
+      .populate("courseId");
+    return successWithData(
+      res,
+      OK,
+      "certificates retrieved successfully",
+      userCertificates
+    );
+  } catch (err) {
+    console.log(err);
+    return AppError.tryCatchError(res, err);
+  }
+};
 
-    try {
-        const query = req.body.id;
-        const userCertificates = await Certificate.find({ userId: query });
-        return successWithData(
-            res,
-            OK,
-            "searched successfully",
-            userCertificates
-        );
-
-    } catch (err) {
-        console.log(err);
-        return AppError.tryCatchError(res, err);
-    }
+exports.getCourseCertificate = async (req, res, next) => {
+  try {
+    const courseCertificates = await Certificate.find({
+      courseId: req.params.courseId
+    })
+      .populate("userId")
+      .populate("courseId");
+    return successWithData(
+      res,
+      OK,
+      "certificates retrieved successfully",
+      courseCertificates
+    );
+  } catch (err) {
+    console.log(err);
+    return AppError.tryCatchError(res, err);
+  }
 };
 
 exports.getAllCertificates = async (req, res, next) => {
-    try {
-        const certificates = await Certificate.find({})
-            .sort("-createdAt");
-        return successWithData(res, OK, "Certificates fetched successfully", certificates);
-    } catch (err) {
-        console.log(err);
-        return AppError.tryCatchError(res, err);
+  try {
+    const certificates = await Certificate.find({})
+      .populate("userId")
+      .populate("courseId")
+      .sort("-createdAt");
+    return successWithData(
+      res,
+      OK,
+      "Certificates fetched successfully",
+      certificates
+    );
+  } catch (err) {
+    console.log(err);
+    return AppError.tryCatchError(res, err);
+  }
+};
+
+exports.deleteCertificate = async (req, res, file) => {
+  try {
+    const certificate = await Certificate.findOneAndDelete({
+      _id: req.params.id
+    });
+    if (!certificate) {
+      let error = { message: "undefined Certificate" };
+      return AppError.tryCatchError(res, error);
     }
+    return successNoData(res, OK, "certificate deleted");
+  } catch (err) {
+    console.log(err);
+    return AppError.tryCatchError(res, err);
+  }
 };

0bc4e6833e480800de262126356c11515b17099e
pextech <mcstain1639@gmail.com>
2022-01-13 15:40:50 +0200

    added forgot password functionality

--- controllers/v1/auth/index.js
+++ controllers/v1/auth/index.js
@@ -1,14 +1,18 @@
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 
-import sendEmail from '../../../utils/email/sendEmail';
-import emailTemplate from '../../../utils/email/emailService';
+import sendEmail from "../../../utils/email/sendEmail";
+import emailTemplate from "../../../utils/email/emailService";
 const { UNAUTHORIZED, BAD_REQUEST, OK } = require("http-status-codes");
+
+import Token from "../../../model/v1/Token";
 
 const {
   successWithData,
-  successNoData,
+  successNoData
 } = require("../../../utils/successHandler");
+
+import emailFormatForgot from "../../../utils/email/forgotPassword/email";
 
 // DB
 const User = require("../../../model/v1/User");
@@ -22,11 +26,9 @@ const AppError = require("../../../utils/appError");
 
 const createSendToken = require("./createSendToken");
 
-
-exports.getAllUser= async (req, res, next) => {
+exports.getAllUser = async (req, res, next) => {
   try {
-    const users = await User.find({})
-      .sort("-createdAt");
+    const users = await User.find({}).sort("-createdAt");
     return successWithData(res, OK, "Users fetched successfully", users);
   } catch (err) {
     console.log(err);
@@ -48,9 +50,14 @@ exports.signupUser = async (req, res, next) => {
     }
     const newUser = await User.create({ ...req.body });
 
-    const subject = 'MPA Account Successfuly created!';
+    const subject = "MPA Account Successfuly created!";
     sendEmail(emailTemplate(newUser.firstName), subject, newUser.email);
-    const newNotification = await Notifications.create({receiverId: newUser._id, title: subject, type: 'User', description: 'Please create your account profile'});
+    const newNotification = await Notifications.create({
+      receiverId: newUser._id,
+      title: subject,
+      type: "User",
+      description: "Please create your account profile"
+    });
     const message = `Dear ${newUser.firstName} , Successfully Registered your account! login with your email and password`;
     createSendToken(newUser, 201, res, message);
   } catch (err) {
@@ -68,7 +75,7 @@ exports.loginUser = async (req, res, next) => {
   try {
     const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
+    if (!user) {
       errors.msg = "Email is Incorrect";
       return AppError.validationError(res, UNAUTHORIZED, errors);
     }
@@ -79,5 +86,66 @@ exports.loginUser = async (req, res, next) => {
     createSendToken(user, 201, res, "User Authorized");
   } catch (err) {
     return AppError.validationError(res, err);
+  }
+};
+
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const { errors, isValid } = validateForgotPassword(req.body);
+    if (!isValid) {
+      return AppError.validationError(res, BAD_REQUEST, errors);
+    }
+
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res.status(400).send("user with given email doesn't exist");
+
+    let token = await Token.findOne({ userId: user._id });
+    if (!token) {
+      token = await new Token({
+        userId: user._id,
+        token: crypto.randomBytes(32).toString("hex")
+      }).save();
+    }
+
+    const link = `https://www.minorityprogrammers.com/reset?userId=${user._id}&&token=${token.token}`;
+    const subject = "Link to Reset your password!";
+    sendEmail(emailFormatForgot(user.firstName, link), subject, user.email);
+
+    return successNoData(
+      res,
+      OK,
+      "password reset link sent to your email account"
+    );
+  } catch (error) {
+    res.send("An error occured");
+    console.log(error);
+  }
+};
+
+exports.resetPassword = async (req, res, next) => {
+  try {
+    const { errors, isValid } = validateResetPassword(req.body);
+    if (!isValid) {
+      return AppError.validationError(res, BAD_REQUEST, errors);
+    }
+
+    const user = await User.findById(req.params.userId);
+    if (!user) return res.status(400).send("invalid link or expired");
+
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token
+    });
+    if (!token) return res.status(400).send("Invalid link or expired");
+
+    user.password = req.body.password;
+    await user.save();
+    await token.delete();
+
+    return successNoData(res, OK, "User Password Resetted successfully");
+  } catch (error) {
+    res.send("An error occured");
+    console.log(error);
   }
 };

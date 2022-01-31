351e80e2ec9b5d7e3f070ab064f0d6051cf70e83
pextech <mcstain1639@gmail.com>
2022-01-31 10:53:58 +0200

    changed wallet provider

--- utils/serverWallet.js
+++ utils/serverWallet.js
@@ -2,7 +2,7 @@ const ethers = require("ethers");
 
 const createWallet = () => {
   let provider = new ethers.providers.JsonRpcProvider(
-    `https://polygon-mumbai.infura.io/v3/${process.env.API_PROVIDER}`
+    `https://polygon-mainnet.infura.io/v3/${process.env.API_PROVIDER}`
   );
 
   let wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
